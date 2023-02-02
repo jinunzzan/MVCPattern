a4fa5e91c6123a0b0613752bd01cf2d12358d148
jinunzzan <lovebox6607@gmail.com>
2023-02-02 18:50:43 +0900

    Initial Commit

---
+++ MVCPattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVCPattern
+//
+//  Created by Eunchan Kim on 2023/02/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
